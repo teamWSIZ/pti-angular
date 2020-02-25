79e3429c81a5c09287bf289f5a58a014bdabe253
Piotr Marecki <pm@aimachina.io>
2020-02-25 14:37:36 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gg';
+  //klasa z danymi
+  gg : string = 'Abra kadabra';
+  x = 12;
+
+
+  funkcja() {
+    let z = 111;  //zmienna lokalna
+    this.x += 1;
+  }
+
 }
